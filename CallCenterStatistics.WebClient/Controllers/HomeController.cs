69eb91936dd35014f85d06068513cea7547bd10b
Konstantin <you@example.com>
2019-04-21 19:49:44 +0300

    csv file upload

--- CallCenterStatistics.WebClient/Controllers/HomeController.cs
+++ CallCenterStatistics.WebClient/Controllers/HomeController.cs
@@ -1,17 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CallCenterStatistics.WebClient.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace CallCenterStatistics.WebClient.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private const string FileExtension = ".csv";
+        
+        public async Task<IActionResult> Index(IFormFile file)
         {
+            if (file == null || file.Length <= 0)
+                ModelState.AddModelError(string.Empty, "File should be chosen");
+            
+            if(Path.GetExtension(file?.FileName) != FileExtension)
+                ModelState.AddModelError(string.Empty, $"File extension should be '{FileExtension}'");
+
+            if (!ModelState.IsValid) return View();
+
+            var filePath = Path.GetTempFileName();
+            
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            
+            ViewData["FileName"] = Path.GetFileName(filePath);
             return View();
         }
 
--- CallCenterStatistics.WebClient/Program.cs
+++ CallCenterStatistics.WebClient/Program.cs
@@ -19,6 +19,7 @@ namespace CallCenterStatistics.WebClient
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseKestrel(options => { options.Limits.MaxRequestBodySize = null; });
     }
 }
